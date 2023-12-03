efd14bbc776b3e572ee66aaa77391860759ff06a
vimjoyer <140887703+vimjoyer@users.noreply.github.com>
2023-12-03 21:04:19 +0100

    Create flake.nix

---
+++ default/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+    
+      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+          extraSpecialArgs = {inherit inputs;};
+          modules = [ 
+            ./configuration.nix
+            # inputs.home-manager.nixosModules.default
+          ];
+        };
+
+    };
+}
