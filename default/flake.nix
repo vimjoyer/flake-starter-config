818ebff26ed186e5635c71e7d031ef4d5d2882f0
vimjoyer <140887703+vimjoyer@users.noreply.github.com>
2024-03-10 14:34:27 +0100

    identation fix

--- default/flake.nix
+++ default/flake.nix
@@ -10,16 +10,13 @@
     # };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
-    {
-    
-      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-          specialArgs = {inherit inputs;};
-          modules = [ 
-            ./configuration.nix
-            # inputs.home-manager.nixosModules.default
-          ];
-        };
-
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./configuration.nix
+        # inputs.home-manager.nixosModules.default
+      ];
     };
+  };
 }
